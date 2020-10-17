e6991fb7785287763e437048c98a5a1d3680a554
andywar65 <andy.war1965@gmail.com>
2020-10-17 18:04:13 +0200

    changed from file browser to image field to allow command  to work

--- management/commands/fetch_portfolio_emails.py
+++ management/commands/fetch_portfolio_emails.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 from imap_tools import MailBox, AND
 
-from portfolio.models import Project
+from portfolio.models import Project, ProjectImage
 
 class Command(BaseCommand):
 
@@ -19,9 +19,19 @@ class Command(BaseCommand):
         with MailBox(HOST).login(USER, PASSWORD, 'INBOX') as mailbox:
             for message in mailbox.fetch(AND(seen=False, subject='progetti',
                 from_=FROM), mark_seen=False):
-                pass
-                #for att in message.attachments:  # list: [Attachment objects]
-                    #file = SimpleUploadedFile(att.filename, att.payload,
-                        #att.content_type)
-                    #instance = CSVInvoice(csv=file)
-                    #instance.save()
+                msg = message.text
+                msg = msg.replace('TITOLO[', '')
+                title = msg.split(']', 1)[0]
+                msg = msg.split(']', 1)[1]
+                msg = msg.replace('DESCRIZIONE[', '')
+                intro = msg.split(']', 1)[0]
+                prog = Project(title=title, intro=intro)
+                prog.save()
+                for att in message.attachments:  # list: [Attachment objects]
+                    file = SimpleUploadedFile(att.filename, att.payload,
+                        att.content_type)
+                    position = att.filename.split('-', 1)[0]
+                    caption = att.filename.split('-', 1)[1]
+                    instance = ProjectImage(prog_id=prog.id, image=file,
+                        position=position, caption=caption)
+                    instance.save()
--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.timezone import now
 
-from filebrowser.fields import FileBrowseField
+#from filebrowser.fields import FileBrowseField
 
 class Project(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -31,9 +31,9 @@ class Project(models.Model):
 class ProjectImage(models.Model):
     prog = models.ForeignKey(Project, on_delete = models.CASCADE,
         related_name='article_uploads')
-    image = FileBrowseField("Immagine", max_length=200,
-        extensions=[".jpg", ".png", ".jpeg", ".gif", ".tif", ".tiff"],
-        null=True, directory='portfolio/projects/')
+    image = models.ImageField("Immagine", max_length=200,
+        #extensions=[".jpg", ".png", ".jpeg", ".gif", ".tif", ".tiff"],
+        null=True, upload_to='uploads/portfolio/projects/')#directory='portfolio/projects/')
     caption = models.CharField("Didascalia", max_length = 200, blank=True,
         null=True)
     position = models.PositiveSmallIntegerField("Position", null=True)
