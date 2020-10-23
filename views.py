7b6728c2e4c65d4bf4255ca3c7c75519b4095f9a
andywar65 <andy.war1965@gmail.com>
2020-10-23 19:26:14 +0200

    working project category list view and template

--- views.py
+++ views.py
@@ -8,6 +8,7 @@ from imap_tools import MailBox, AND
 from .models import Project
 from pages.models import GalleryImage
 from .management.commands.fetch_portfolio_emails import do_command
+from .choices import *
 
 class ProjectListView(ListView):
     model = Project
@@ -32,6 +33,39 @@ class ProjectCategoryListView(ListView):
     context_object_name = 'progs'
     template_name = 'portfolio/project_category_list.html'
     paginate_by = 12
+
+    def get_readable(self, list, target):
+        for i in list:
+            if i[0] == target:
+                return i[1]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if 'category' in self.request.GET:
+            readable = self.get_readable(CATEGORY, self.request.GET["category"])
+            context['cat_filter'] = f'in Categoria: {readable}'
+        elif 'type' in self.request.GET:
+            readable = self.get_readable(TYPE, self.request.GET["type"])
+            context['cat_filter'] = f'di Intervento: {readable}'
+        elif 'status' in self.request.GET:
+            readable = self.get_readable(STATUS, self.request.GET["status"])
+            context['cat_filter'] = f'con Status: {readable}'
+        elif 'cost' in self.request.GET:
+            readable = self.get_readable(COST, self.request.GET["cost"])
+            context['cat_filter'] = f'di Costo: {readable}'
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if 'category' in self.request.GET:
+            qs = qs.filter(category=self.request.GET['category'])
+        elif 'type' in self.request.GET:
+            qs = qs.filter(type=self.request.GET['type'])
+        elif 'status' in self.request.GET:
+            qs = qs.filter(status=self.request.GET['status'])
+        elif 'cost' in self.request.GET:
+            qs = qs.filter(cost=self.request.GET['cost'])
+        return qs
 
 class ProjectDetailView(DetailView):
     model = Project
