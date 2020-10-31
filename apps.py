6ad7d3de55a61715501bd43d441325ecc83e060e
andywar65 <andy.war1965@gmail.com>
2020-10-31 23:59:18 +0100

    adding permissions on migration

--- apps.py
+++ apps.py
@@ -5,9 +5,12 @@ def create_portfolio_group(sender, **kwargs):
     from django.contrib.auth.models import Permission, Group
     grp, created = Group.objects.get_or_create(name='Project Manager')
     if created:
-        permission = Permission.objects.filter(codename__in=("view_project",
-            'add_project', 'change_project', 'delete_project', ))
-        grp.permissions.set(permission)
+        permissions = Permission.objects.filter(codename__in=('view_project',
+            'add_project', 'change_project', 'delete_project',
+            'view_projectstation', 'add_projectstation', 'change_projectstation',
+            'delete_projectstation', 'view_stationimage', 'add_stationimage',
+            'change_stationimage', 'delete_stationimage', ))
+        grp.permissions.set(permissions)
 
 class PortfolioConfig(AppConfig):
     name = 'portfolio'
