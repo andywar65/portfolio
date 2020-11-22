edd0db1c7a149a3e18b46c287ee790b78c975d22
andywar65 <andy.war1965@gmail.com>
2020-11-22 23:49:23 +0100

    added model tests for projects and projectstations

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,44 @@
-from django.test import TestCase
+from datetime import datetime
+
+from django.conf import settings
+from django.test import TestCase, override_settings
 
 from portfolio.models import Project, ProjectStation, StationImage
 
+@override_settings(USE_I18N=False)
 class ProjectModelTest(TestCase):
     """Testing all methods that don't need SimpleUploadedFile"""
     @classmethod
     def setUpTestData(cls):
-        Project.objects.create(title='Project', intro = 'Foo',
+        prog = Project.objects.create(title='Project', intro = 'Foo',
             body = 'Bar', site = 'Somewhere', category = 'ALT',
             type = 'ALT', status = 'ALT', cost = 'ALT', )
+        Project.objects.create(date=datetime.strptime('2020-05-09', '%Y-%m-%d'))
+        stat = ProjectStation.objects.create(prog=prog, title='Station')
+        #StationImage.objects.create(stat_id=stat.id,
+            #image='uploads/images/galleries/image.jpg')
 
     def test_project_str_method(self):
         prog = Project.objects.get(slug='project')
         self.assertEquals(prog.__str__(), 'Project')
+
+    def test_project_str_method_no_title(self):
+        prog = Project.objects.get(date='2020-05-09')
+        self.assertEquals(prog.__str__(), 'Project-09-05-20')
+
+    def test_project_get_full_path(self):
+        prog = Project.objects.get(slug='project')
+        self.assertEquals(prog.get_full_path(), '/projects/project/')
+
+    def test_projectstation_str_method(self):
+        stat = ProjectStation.objects.get(slug='station')
+        self.assertEquals(stat.__str__(), 'Station / Project')
+
+    def test_projectstation_intro(self):
+        stat = ProjectStation.objects.get(slug='station')
+        self.assertEquals(stat.intro,
+            f'Another photo station by {settings.WEBSITE_NAME}!')
+
+    #def test_stationimage_fb_image(self):
+        #image = StationImage.objects.get(image='uploads/images/galleries/image.jpg')
+        #self.assertEquals(image.fb_image, 'uploads/images/galleries/image.jpg')
