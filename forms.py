a7c8a322fc14585719e422e46452eb7f50253826
andywar65 <andy.war1965@gmail.com>
2020-12-21 23:55:51 +0100

    deleting forms

--- forms.py
+++ forms.py
@@ -2,27 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from django.utils.translation import gettext as _
 
-from .models import Project, ProjectStation, StationImage, ProjectMapDxf
+from .models import Project
 
-class ProjectStationCreateForm(ModelForm):
-    prog = forms.ModelChoiceField( label=_('Project'),
-        queryset=Project.objects.all(), disabled = True )
-
-    class Meta:
-        model = ProjectStation
-        fields = '__all__'
-
-class ProjectMapDxfCreateForm(ModelForm):
-    prog = forms.ModelChoiceField( label=_('Project'),
-        queryset=Project.objects.all(), disabled = True )
-
-    class Meta:
-        model = ProjectMapDxf
-        fields = '__all__'
-
-class StationImageCreateForm(ModelForm):
-    image = forms.ImageField(label=_('Image'), required=True)
-
-    class Meta:
-        model = StationImage
-        fields = ( 'stat', 'date', 'image', 'caption')
+#your forms here
