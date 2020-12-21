8df93949759e9e9ccd9ae4628878679412ea5ad2
andywar65 <andy.war1965@gmail.com>
2020-12-21 23:51:57 +0100

    deleting urlpatterns

--- urls.py
+++ urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
 from .views import (ProjectListView, ProjectYearArchiveView,
-    ProjectCategoryListView, ProjectDetailView, ProjectStationListView,
-    ProjectStationDetailView, ProjectStationCreateView,
-    StationImageDayArchiveView, StationImageCreateView, ProjectMapDxfCreateView)
+    ProjectCategoryListView, ProjectDetailView, )
 
 app_name = 'portfolio'
 urlpatterns = [
@@ -13,16 +11,4 @@ urlpatterns = [
     path(_('category/'), ProjectCategoryListView.as_view(),
         name = 'project_category'),
     path('<slug>/', ProjectDetailView.as_view(), name = 'project_detail'),
-    path(_('<slug>/stations/'), ProjectStationListView.as_view(),
-        name = 'station_list'),
-    path(_('<slug>/stations/add/'), ProjectStationCreateView.as_view(),
-        name = 'station_create'),
-    path(_('<slug>/stations/dxf/'), ProjectMapDxfCreateView.as_view(),
-        name = 'dxf_create'),
-    path(_('<slug:prog_slug>/stations/<slug:stat_slug>/'),
-        ProjectStationDetailView.as_view(), name = 'station_detail'),
-    path(_('<slug:prog_slug>/stations/<slug:stat_slug>/add/'),
-        StationImageCreateView.as_view(), name = 'image_add'),
-    path(_('<slug>/stations/<int:year>/<int:month>/<int:day>/'),
-        StationImageDayArchiveView.as_view(), name = 'image_day'),
     ]
