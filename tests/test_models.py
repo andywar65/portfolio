004511dff899c1d53c062efe746019732aa4a9aa
andywar65 <andy.war1965@gmail.com>
2020-12-30 18:06:34 +0100

    fixed tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.test import TestCase, override_settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from portfolio.models import Project, ProjectStation, StationImage
+from portfolio.models import Project
 
 @override_settings(USE_I18N=False)
 class ProjectModelTest(TestCase):
@@ -16,7 +16,7 @@ class ProjectModelTest(TestCase):
             body = 'Bar', site = 'Somewhere', category = 'ALT',
             type = 'ALT', status = 'ALT', cost = 'ALT', )
         Project.objects.create(date=datetime.strptime('2020-05-09', '%Y-%m-%d'))
-        stat = ProjectStation.objects.create(prog=prog, title='Station')
+        #stat = ProjectStation.objects.create(prog=prog, title='Station')
         #StationImage.objects.create(stat_id=stat.id,
             #image='uploads/images/galleries/image.jpg')
 
@@ -31,42 +31,3 @@ class ProjectModelTest(TestCase):
     def test_project_get_full_path(self):
         prog = Project.objects.get(slug='project')
         self.assertEquals(prog.get_full_path(), '/projects/project/')
-
-    def test_projectstation_str_method(self):
-        stat = ProjectStation.objects.get(slug='station')
-        self.assertEquals(stat.__str__(), 'Station / Project')
-
-    def test_projectstation_intro(self):
-        stat = ProjectStation.objects.get(slug='station')
-        self.assertEquals(stat.intro,
-            f'Another photo station by {settings.WEBSITE_NAME}!')
-
-@override_settings(MEDIA_ROOT=os.path.join(settings.MEDIA_ROOT, 'temp'))
-class StationImageTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        prog = Project.objects.create(title='Project', intro = 'Foo',
-            body = 'Bar', site = 'Somewhere', category = 'ALT',
-            type = 'ALT', status = 'ALT', cost = 'ALT', )
-        stat = ProjectStation.objects.create(prog=prog, title='Station')
-        img_path = os.path.join(settings.STATIC_ROOT,
-            'portfolio/sample/image.jpg')
-        with open(img_path, 'rb') as f:
-            content = f.read()
-        statimg = StationImage.objects.create(stat_id=stat.id,
-            image=SimpleUploadedFile('image.jpg', content, 'image/jpg'))
-
-    def tearDown(self):
-        """Checks existing files, then removes them"""
-        try:
-            list = os.listdir(os.path.join(settings.MEDIA_ROOT,
-                'uploads/images/galleries/'))
-        except:
-            return
-        for file in list:
-            os.remove(os.path.join(settings.MEDIA_ROOT,
-                f'uploads/images/galleries/{file}'))
-
-    def test_stationimage_fb_image(self):
-        image = StationImage.objects.get(image='uploads/images/galleries/image.jpg')
-        self.assertEquals(image.fb_image.path, 'uploads/images/galleries/image.jpg')
